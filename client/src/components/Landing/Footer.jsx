30fd536b5fff75cbab21dad7bcfd4ec257ced827
Samridh singh <69411088+thisissamridh@users.noreply.github.com>
2023-11-26 12:21:38 +0530

    Update Footer.jsx

--- client/src/components/Landing/Footer.jsx
+++ client/src/components/Landing/Footer.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-primary text-center py-4 text-white">© 2023 WatchTower. All rights reserved.</footer>
-
-  )
+    <footer className="bg-primary text-center py-4 text-white">
+      © 2023 WatchTower. All rights reserved.
+      <br/>
+      Mentor: <a href="https://www.linkedin.com/in/mohd-mohsin-4974541bb/" target="_blank" rel="noopener noreferrer">Mohd Mohsin</a>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
