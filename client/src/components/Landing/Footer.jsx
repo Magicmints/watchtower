eea202a530dd6b7ab0b6e1c49929472181ff574c
Samridh singh <69411088+thisissamridh@users.noreply.github.com>
2024-01-28 21:02:33 +0530

    Update Footer.jsx m

--- client/src/components/Landing/Footer.jsx
+++ client/src/components/Landing/Footer.jsx
@@ -4,16 +4,6 @@ const Footer = () => {
   return (
     <footer className="bg-primary text-center py-4 text-white">
       © 2023 WatchTower. All rights reserved.
-      <br/>
-      <span style={{ fontWeight: 'bold', color: 'yellow' }}>Mentor: </span>
-      <a 
-        href="https://www.linkedin.com/in/mohd-mohsin-4974541bb/" 
-        target="_blank" 
-        rel="noopener noreferrer"
-        style={{ fontWeight: 'bold', color: 'yellow' }}
-      >
-        Mohd Mohsin Sir
-      </a>
     </footer>
   );
 }
